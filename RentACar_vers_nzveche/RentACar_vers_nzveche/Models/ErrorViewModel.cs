e91481cd6aef26f3d5279ebde47cc914385402b3
ValsCodes <39241389+ValsCodes@users.noreply.github.com>
2022-03-27 04:27:47 -0800

    Framework 5.0

---
+++ RentACar_vers_nzveche/RentACar_vers_nzveche/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RentACar_vers_nzveche.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
